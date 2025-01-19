76322d3e08fd16b3554d93f1e1ce4df9b8810eb3
bugsymangesh23 <emmanuelmangera08@gmail.com>
2025-01-19 12:41:38 +0300

    test

--- guessy_game/game/views.py
+++ guessy_game/game/views.py
@@ -1,4 +1,5 @@
 import random
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from .models import Game
 
@@ -8,48 +9,51 @@ def get_secret_num():
     random.shuffle(numbers)
     return ''.join(numbers[:3])
 
-def index(request):
-    if request.method == 'POST':
-        # Start a new game
-        secret_number = get_secret_num()
-        Game.objects.create(secret_number=secret_number)
-        return redirect('play')
-
-    return render(request, 'index.html')
-
-def play(request):
-    game = Game.objects.last() # Get latest game instance
-
-    if request.method == 'POST':
-        guess = request.POST.get('guess')
-        clues = get_clues(guess, game.secret_number)
-
-        if guess == game.secret_number:
-            return render(request, 'result.html', {'result': 'You got it!', 'clues': clues})
-
-        game.guesses += 1
-        game.save()
-
-        if game.guesses >= 10:
-            return render(request, 'result.html', {'resuult': 'Game Over!', 'clues': f'The number was {game.secret_number}'})
-
-        return render(request, 'play.html', {'clues': clues})
-
-    return render(request, 'play.html')
-
 def get_clues(guess, secret_number):
-    """Retuens clues based on the player's guess."""
+    """Returns clues based on the player's guess."""
     if guess == secret_number:
         return ['You got it!']
 
     clues = []
     for i in range(len(guess)):
         if guess[i] == secret_number[i]:
-            clues.append('Woza') # correct digit in correct position
+            clues.append('Woza')  # Correct digit in the correct position
         elif guess[i] in secret_number:
-            clues.append('Chop rice!') # correct digit but wrong position
+            clues.append('Chop_rice')  # Correct digit but in the wrong position
+
     if not clues:
-        return ['Yooh!'] #No correct digits
+        return ['Yooh!']  # No correct digits
 
     return clues
+
+def index(request):
+    if request.method == 'POST':
+        # Start a new game
+        secret_number = get_secret_num()
+        Game.objects.create(secret_number=secret_number, guesses=0)
+        return redirect('play')
+
+    return render(request, 'index.html')
+
+def play(request):
+    game = Game.objects.last()  # Get the latest game instance
+
+    if request.method == 'POST':
+        guess = request.POST.get('guess')
+        clues = get_clues(guess, game.secret_number)
+
+        if guess == game.secret_number:
+            messages.success(request, f'You got it! The secret number was {game.secret_number}')
+            return redirect('index')  # Redirect to index after winning
+        
+        game.guesses += 1
+        game.save()
+
+        if game.guesses >= 10:
+            messages.info(request, f'Game Over! The correct number was {game.secret_number}')
+            return redirect('index')  # Redirect to index after game over
+
+        return render(request, 'play.html', {'clues': clues})
+
+    return render(request, 'play.html')
 
