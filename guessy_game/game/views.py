8066eecc72d09a1882c3cc51599258c9cd8b8cc1
bugsymangesh23 <emmanuelmangera08@gmail.com>
2025-01-19 11:27:10 +0300

    feat:Created the views

--- guessy_game/game/views.py
+++ guessy_game/game/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+import random
+from django.shortcuts import render, redirect
+from .models import Game
 
-# Create your views here.
+def get_secret_num():
+    """Generates a secret number with unique digits."""
+    numbers = list('0123456789')
+    random.shuffle(numbers)
+    return ''.join(numbers[:3])
+
+def index(request):
+    if request.method == 'POST':
+        # Start a new game
+        dsecret_number = get_secret_num()
+        Game.objects.create(secret_number=secret_number)
+        return redirect('play')
+
+    return render(request, 'index.html')
+
+def play(request):
+    game = Game.objects.last() # Get latest game instance
+
+    if request.method == 'POST':
+        guess = request.POST.get('guess')
+        clues = get_clues(guess, game.secret_number)
+
+        if guess = game.secret_number:
+            return render(request, 'result.html', {'result': 'You got it!', 'clues': clues})
+
+        game.guesses += 1
+        game.save()
+
+        if game.guesses >= 10:
+            return render(request, 'result.html', {'resuult': 'Game Over!', 'clues': f'The number was {game.secret_number}'})
+
+        return render(request, 'play.html', {'clues': clues})
+
+    return render(request, 'play.html')
+
+def get_clues(guess, secret_number):
+    """Retuens clues based on the player's guess."""
+    if guess[i] == secret_number[i]:
+        clues.append('Woza') # correct digit in correct position
+    elif guess[i] in secret_number:
+        clues.append('Chop rice!') # correct digit but wrong position
+        if not clues:
+            return ['Yooh!'] #No correct digits
+
+        return clues
+
